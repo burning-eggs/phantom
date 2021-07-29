aac838eca12662973a8b5f2dd4bd0b0c20a9f90d
flying-dojo <pro-uno-player@protonmail.com>
2021-07-29 18:26:30 +0300

    get() window

--- src/main/java/org/phantom/Window.java
+++ src/main/java/org/phantom/Window.java
@@ -1,5 +1,22 @@
 package org.phantom;
 
 public class Window {
-    // TODO: Create window
+    private int width, height;
+    private String title;
+
+    private static Window window = null;
+
+    private Window() {
+        this.width = 1920;
+        this.height = 1080;
+        this.title = "phantom engine";
+    }
+
+    public static Window get() {
+        if (Window.window == null) {
+            Window.window = new Window();
+        }
+
+        return Window.window;
+    }
 }
