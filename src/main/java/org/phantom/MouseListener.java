5bb125d73ea7fd7b04793f186f97f8db4f906981
flying-dojo <pro-uno-player@protonmail.com>
2021-07-29 22:11:21 +0300

    get() mouselistener

---
+++ src/main/java/org/phantom/MouseListener.java
@@ -0,0 +1,26 @@
+package org.phantom;
+
+public class MouseListener {
+    private static MouseListener instance;
+    private double scrollX, scrollY;
+    private double xPos, yPos, lastY, lastX;
+    private boolean mouseButtonPressed[] = new boolean[3];
+    private boolean isDragging;
+
+    private MouseListener() {
+        this.scrollX = 0.0;
+        this.scrollY = 0.0;
+        this.xPos = 0.0;
+        this.yPos = 0.0;
+        this.lastX = 0.0;
+        this .lastY = 0.0;
+    }
+
+    public static MouseListener get() {
+        if (MouseListener.instance == null) {
+            MouseListener.instance = new MouseListener();
+        }
+
+        return MouseListener.instance;
+    }
+}
