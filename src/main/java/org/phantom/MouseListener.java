b6946dc7e44ecfedf583c9eb865672b3737628a6
flying-dojo <87005812+flying-dojo@users.noreply.github.com>
2021-07-30 18:33:42 +0300

    mouselistener copyright

--- src/main/java/org/phantom/MouseListener.java
+++ src/main/java/org/phantom/MouseListener.java
@@ -1,3 +1,12 @@
+/*
+ * Copyright (C) 2021-2021 | flying-dojo | All rights reserved.
+ *
+ * This source code is subject to the terms of the GNU General Public
+ * License, version 3. If a copy of the GPL was not distributed with this
+ * file, you can obtain one at: https://gnu.org/licenes/gpl-3.0.txt
+ */
+
+
 package org.phantom;
 
 import static org.lwjgl.glfw.GLFW.GLFW_PRESS;
